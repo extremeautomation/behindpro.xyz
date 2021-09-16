92161cae4141ec2e005b128f17603d90ce287313
Vadim Adamovich <vadim@aestasit.com>
2021-09-16 16:54:27 +0200

    unhandled error

--- src/layout/layoutinner.js
+++ src/layout/layoutinner.js
@@ -4,19 +4,50 @@ import Header from "./header"
 import Footer from "./footer"
 import "../css/index.css"
 
-import Accordion from '@material-ui/core/Accordion';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Accordion from "@material-ui/core/Accordion";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { withStyles } from "@material-ui/core/styles";
+
+Accordion = withStyles({
+  root: {
+    width: "20%",
+    backgroundColor: "#f55f74",
+    padding: "10px"
+  },
+  "&:hover": {
+    background: "#3b4871"
+  }
+})
+
+AccordionSummary = withStyles({
+  root: {
+    backgroundColor: "#d53f54"
+  },
+  "&:hover": {
+    background: "#3b4871"
+  }
+})
+
+AccordionDetails = withStyles({
+  root: {
+    display: "block",
+    backgroundColor: "#5b6891"
+  },
+  "&:hover": {
+    background: "#8b98c1"
+  }
+})
 
 const LayoutInner = ({ pageContext: { data }}) => {
   return(<>
            <Header/>
            <div>
              <menu>
-               {data.map(topitem => <Accordion square={true} className={"accordion"}>
-                                      <AccordionSummary expandIcon={<ExpandMoreIcon/>} className={"accordionSummary"}>{topitem.title}</AccordionSummary>
-                                      <AccordionDetails className={"accordionDetails"}>
+               {data.map(topitem => <Accordion square={true}>
+                                      <AccordionSummary expandIcon={<ExpandMoreIcon/>}>{topitem.title}</AccordionSummary>
+                                      <AccordionDetails>
                                         {topitem.tools.map((subitem, index) => <li key={index}><Link to={`/${subitem}`} className={"tools"}>{subitem}</Link></li>)}
                                       </AccordionDetails>
                                     </Accordion>)}
